0ab7b2b649df1fdb849eb68be48fc6ed4cef1a75
FRBallero <fr.ballero@alumnos.upm.es>
2015-04-14 13:14:21 +0200

    random versión 1

---
+++ random.js
@@ -0,0 +1,10 @@
+//Fco Rómulu Ballero Garijo, 51116125M; Mateo Cámara Largo, 51110718A
+var numero = Math.random();
+
+var str =' MAYOR que 0,5';
+
+if (numero <= 0.5){
+	str =' MENOR que 0,5';
+}
+
+console.log('\n' + numero + str + '\n');
